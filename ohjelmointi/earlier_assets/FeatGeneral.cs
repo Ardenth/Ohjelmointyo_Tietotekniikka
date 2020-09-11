be36b92c920bee9dc18d4a8c3df3138f75087918
Ardenth <lehkonen.kortex.jyu.fi@gmail.com>
2020-09-12 00:26:00 +0300

    Revert "asd"
    This reverts commit eb9fa8187bd39bbffb26d99b7f6a73cf3f04af38.

---
+++ ohjelmointi/earlier_assets/FeatGeneral.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class FeatGeneral : MonoBehaviour
+{
+    [XmlElement("FeatName")]
+    public string featname;
+
+    [XmlElement("Level")]
+    public float level;
+
+    [XmlElement("Prerequisite")]
+    public string prerequisite;
+
+    [XmlElement("Description")]
+    public string description;
+
+}
