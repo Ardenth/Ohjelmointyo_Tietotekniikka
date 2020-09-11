f4d56ae9101278d80c256af624cc2f56ce30c3a9
Ardenth <lehkonen.kortex.jyu.fi@gmail.com>
2020-09-12 00:40:45 +0300

    Revert "asd"
    This reverts commit eb9fa8187bd39bbffb26d99b7f6a73cf3f04af38.

---
+++ ohjelmointi/earlier_assets/FeatGeneralContainer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+ [XmlRoot("FeatGeneralCollection")]
+public class FeatGeneralContainer 
+{
+
+    [XmlArray("FeatsGeneral")]
+    [XmlArrayItem("FeatGeneral")]
+    public List<FeatGeneral> feats = new List<FeatGeneral>();
+
+    public static FeatGeneralContainer Load(string path)
+    {
+        TextAsset _xml = Resources.Load<TextAsset>(path);
+
+        XmlSerializer serializer = new XmlSerializer(typeof(FeatGeneralContainer));
+
+        StringReader reader = new StringReader(_xml.text);
+
+        FeatGeneralContainer feats = serializer.Deserialize(reader) as FeatGeneralContainer;
+
+
+        reader.Close();
+
+        return feats;
+    }
+}
---
+++ ohjelmointi/earlier_assets/FeatGeneralLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatGeneralLoader : MonoBehaviour
+{
+
+    public const string path = "featsgeneral.xml";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         FeatGeneralContainer fgc = FeatGeneralContainer.Load(path);
+
+        foreach(FeatGeneral feat in fgc.feats)
+        {
+            print(feat.featname);
+        }
+    }
+
+}
